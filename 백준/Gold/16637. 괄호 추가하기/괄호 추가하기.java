0baf3be348764565cde61c12de509478b0c2696b
조용진 <101401582+Brio-yj@users.noreply.github.com>
2025-08-04 20:28:59 +0900

    [Gold III] Title: 괄호 추가하기, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,9 +1,9 @@
+
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int N;
-    static int res, max;
+    
+    static int N,max;
     static char cArr[];
 
     public static void main(String[] args) throws IOException {
@@ -11,93 +11,41 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         cArr = new char[N];
         String s = br.readLine();
-        for (int i = 0; i < N; i++) {
-            cArr[i] = s.charAt(i);
-        }
-        res = cArr[0] - '0';
+        for (int i = 0; i < N; i++) {cArr[i] = s.charAt(i);}
         max = Integer.MIN_VALUE;
-        if(N==1){
-            System.out.println(s.charAt(0)-'0');
-            return;
-        }
-        if(N==3){
-            System.out.println(cal(0,1,2));;
-            return;
-        }
-        solve(1);
+        
+        int res = cArr[0] - '0';
+        solve(1,res);
+        
+        if(N==1){max = res;}        
         System.out.println(max);
     }
 
-    static void solve(int idx) {   //괄호 안친다 =0, 친다=1
+    static void solve(int idx,int res) {
         if (idx == N) {
             max = Math.max(max, res);
             return;
         }
-        if (idx % 2 == 1) {
-            //안치기
-            if (cArr[idx] == '+') {
-                if(idx+2 <= N) {
-                    res += (cArr[idx + 1] - '0');
-                    solve(idx + 2);
-                    res -= (cArr[idx + 1] - '0');
-                }
-                if (idx + 4 <= N) {
-                    res += cal(idx + 1, idx + 2, idx + 3);
-                    solve(idx + 4);
-                    res -= cal(idx + 1, idx + 2, idx + 3);
-                }
-            }
-            if (cArr[idx] == '-') {
-                if(idx+2<=N) {
-                    res -= (cArr[idx + 1] - '0');
-                    solve(idx + 2);
-                    res += (cArr[idx + 1] - '0');
-                }
-                if (idx + 4 <= N) {
-                    res -= cal(idx + 1, idx + 2, idx + 3);
-                    solve(idx + 4);
-                    res += cal(idx + 1, idx + 2, idx + 3);
-                }
-            }
-            if (cArr[idx] == '*') {
-                if(idx+2<=N) {
-                    int temp = res;
-                    res *= (cArr[idx + 1] - '0');
-                    solve(idx + 2);
-                    if((cArr[idx + 1] - '0')==0){
-                        res=temp;
-                    }else {
-                        res /= (cArr[idx + 1] - '0');
-                    }
-                }
-                if (idx + 4 <= N) {
-                    int temp = res;
-                    res *= cal(idx + 1, idx + 2, idx + 3);
-                    solve(idx + 4);
-                    if((cArr[idx + 1] - '0')==0 || cal(idx + 1, idx + 2, idx + 3)==0 ){
-                        res=temp;
-                    }else {
-                        res /= cal(idx + 1, idx + 2, idx + 3);
-                    }
-                }
-            }
+        
+        int nv = cal(res,cArr[idx],cArr[idx+1]-'0');
+        solve(idx+2,nv);
+
+        if(idx+4<=N) {
+            nv = cal(res,cArr[idx],cal(cArr[idx+1]-'0', cArr[idx+2], cArr[idx +3]-'0'));
+            solve(idx+4,nv);
         }
     }
 
-    static int cal(int idx1, int idx2, int idx3) {
-        if (cArr[idx2] == '+') {
-            return (cArr[idx1]-'0') + (cArr[idx3]-'0');
+    static int cal(int num1, char op, int num2) {
+        if (op == '+') {
+            return num1 + num2;
         }
-        if (cArr[idx2] == '-') {
-            return (cArr[idx1]-'0') - (cArr[idx3]-'0');
+        if (op == '-') {
+            return num1 - num2;
         }
-        if (cArr[idx2] == '*') {
-            return (cArr[idx1]-'0') * (cArr[idx3]-'0');
+        if (op == '*') {
+            return num1 * num2;
         }
         return 0;
     }
-    //괄호를 안치면 지금 값이랑 연산자 그다음 숫자랑 연산해서 res 변경 후 idx+2
-    //괄호 치면  1.다음 숫자 다음 연산자 다다음 숫자랑 연산 후 idx+3
-    //근데 괄호 사이 - 나오는거 조심해야하고
-    //3+3+3
 }
