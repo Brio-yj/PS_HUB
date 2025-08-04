90618c865ae4bb01eca84eb9e959763b274575b8
조용진 <101401582+Brio-yj@users.noreply.github.com>
2025-08-04 19:42:47 +0900

    [Gold III] Title: 괄호 추가하기, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int max;
+    static char cArr[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        cArr = new char[N];
+        String s = br.readLine();
+        for (int i = 0; i < N; i++) {
+            cArr[i] = s.charAt(i);
+        }
+        int res = cArr[0] - '0';
+        max = Integer.MIN_VALUE;
+        if(N==1){
+            System.out.println(res);
+            return;
+        }
+        if(N==3){
+            System.out.println(cal(0,1,2));;
+            return;
+        }
+        solve(1,res);
+        System.out.println(max);
+    }
+
+    static void solve(int idx,int res) {   //괄호 안친다 =0, 친다=1
+        if (idx == N) {
+            max = Math.max(max, res);
+            return;
+        }
+        if (idx % 2 == 1) {
+            if (cArr[idx] == '+') {
+                int temp = res;
+                if(idx+2 <= N) {
+                    res += (cArr[idx + 1] - '0');
+                    solve(idx + 2,res);
+                    res=temp;
+                }
+                if (idx + 4 <= N) {
+                    res += cal(idx + 1, idx + 2, idx + 3);
+                    solve(idx + 4,res);
+                    res =temp;
+                }
+            }
+            if (cArr[idx] == '-') {
+                int temp = res;
+                if(idx+2<=N) {
+                    res -= (cArr[idx + 1] - '0');
+                    solve(idx + 2,res);
+                    res = temp;
+                }
+                if (idx + 4 <= N) {
+                    res -= cal(idx + 1, idx + 2, idx + 3);
+                    solve(idx + 4,res);
+                    res = temp;
+                }
+            }
+            if (cArr[idx] == '*') {
+                int temp = res;
+                if(idx+2<=N) {
+                    res *= (cArr[idx + 1] - '0');
+                    solve(idx + 2,res);
+                    res=temp;
+                }
+                if (idx + 4 <= N) {
+                    res *= cal(idx + 1, idx + 2, idx + 3);
+                    solve(idx + 4,res);
+                    res=temp;
+                }
+            }
+        }
+        return ;
+    }
+
+    static int cal(int idx1, int idx2, int idx3) {
+        if (cArr[idx2] == '+') {
+            return (cArr[idx1]-'0') + (cArr[idx3]-'0');
+        }
+        if (cArr[idx2] == '-') {
+            return (cArr[idx1]-'0') - (cArr[idx3]-'0');
+        }
+        if (cArr[idx2] == '*') {
+            return (cArr[idx1]-'0') * (cArr[idx3]-'0');
+        }
+        return 0;
+    }
+    //괄호를 안치면 지금 값이랑 연산자 그다음 숫자랑 연산해서 res 변경 후 idx+2
+    //괄호 치면  1.다음 숫자 다음 연산자 다다음 숫자랑 연산 후 idx+3
+    //근데 괄호 사이 - 나오는거 조심해야하고
+    //3+3+3
+}
