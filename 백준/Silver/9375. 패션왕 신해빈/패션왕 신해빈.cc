3910bb4f540d0c2ff631cfdc5d71698c6fe552c1
조용진 <101401582+Brio-yj@users.noreply.github.com>
2025-08-18 10:24:58 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using stringMap = multimap<string,string>;
+using countMap = map<string,int>;
+using spair = pair<string,string>;
+
+
+void solve(stringMap smap){
+    //국 순열 -1,즉 카운트
+    countMap cntMap;
+    for(const auto &entry : smap){
+        //cout<<"first \t"<<entry.first<<" second"<<entry.second<<"\n";
+        cntMap[entry.first]++;
+    }
+    int product =1;
+    for(const auto &entry : cntMap){
+        //cout<<"first \t"<<entry.first<<" second"<<entry.second<<"\n";
+        product*=(entry.second+1);
+    }
+    cout<<product-1<<"\n";
+}
+
+
+int main(){
+    int k,n;
+    cin>>k;
+    for(int i=0;i<k;i++){
+        cin>>n;
+        stringMap smap;
+        for(int j=0;j<n;j++){
+            string str1,str2;
+            cin>>str2>>str1;
+            smap.insert(spair(str1,str2));
+        }
+        solve(smap);
+    }
+}
