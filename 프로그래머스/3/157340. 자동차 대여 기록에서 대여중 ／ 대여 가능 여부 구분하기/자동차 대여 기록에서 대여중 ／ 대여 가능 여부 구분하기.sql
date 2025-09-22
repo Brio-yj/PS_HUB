6a77549bbb171407f8220b727ad5c23c41dd51c8
조용진 <101401582+Brio-yj@users.noreply.github.com>
2025-09-22 14:23:11 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,11 +1,28 @@
-
-SELECT CAR_ID,
-CASE
-WHEN CAR_ID IN (SELECT CAR_ID
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-WHERE '2022-10-16' BETWEEN START_DATE AND END_DATE) THEN '대여중'
-ELSE '대여 가능'
-END "AVAILABILITY"
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+select
+    CAR_ID,
+    case 
+    when CAR_ID NOT IN(
+        SELECT car_id	
+        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS C
+        WHERE C.start_date <= "2022-10-16" and C.end_date >= "2022-10-16"
+        ) 
+    then "대여 가능"
+    else "대여중"
+    end as AVAILABILITY
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
 GROUP BY CAR_ID
-ORDER BY CAR_ID DESC
+order by CAR_ID DESC
+
+
+
+/*
+a->대여중
+//대여중, 나머진 그냥 대여 가능, 어차피 NOT IN 하면 걸러진다
+
+
+with a as(
+SELECT car_id	
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY AS C
+WHERE C.start_date <= "2022-10-16" and C.end_date >= "2022-10-16"
+)
+*/
